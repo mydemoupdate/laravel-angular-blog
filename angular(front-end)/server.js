2e181d7ab9edc8a9c905792d4c0daf4a12ee90fc
mydemoupdate <mydemoupdate@gmail.com>
2018-11-21 22:26:53 +0700

    change

--- angular(front-end)/server.js
+++ angular(front-end)/server.js
@@ -6,11 +6,11 @@ const app = express();
 
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/angular(front-end)'));
+app.use(express.static(__dirname + '/dist/devupdate2018'));
 
 app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname+ '/dist/angular(front-end)/index.html'));
+  res.sendFile(path.join(__dirname+ '/dist/devupdate2018/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
